f98d3728a02023b608b6f6552d385e94598c6910
burakhezer <96007638+lukadiscrash@users.noreply.github.com>
2022-02-10 05:25:24 +0300

    Add files via upload

---
+++ operematorler/Program.cs
@@ -0,0 +1,75 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(" ***** Atama ve İşlemli Atama Operatörleri ***** ");
+
+// Atama Ve İşlemli Atama
+int x = 3;
+int y = 3;
+y = y+2;
+
+Console.WriteLine(y);
+
+y += 2;
+
+Console.WriteLine(y);
+
+y/=3;
+Console.WriteLine(y);
+x *=2;
+Console.WriteLine(y);
+
+Console.WriteLine(" ***** Mantıksal Operatörler ***** ");
+
+//Mantıksal Operatörler
+// ||, &&, !
+
+bool isSuccess = true;
+bool isCompleted = false;
+
+if(isSuccess && isCompleted)
+  Console.WriteLine("Perfect!");
+
+if(isSuccess || isCompleted)
+  Console.WriteLine("Great!");
+
+if(isSuccess && !isCompleted)
+  Console.WriteLine("Fine!");
+
+
+Console.WriteLine(" ***** İlişkisel Operatörler ***** ");
+// İlişkisel Operatörler
+// <, >, <=, >=, ==, !=
+
+int a = 3;
+int b = 4;
+bool sonuc = a<b;
+
+Console.WriteLine(sonuc);
+sonuc = a>b;
+Console.WriteLine(sonuc);
+sonuc = a>=b;
+Console.WriteLine(sonuc);
+sonuc = a<=b;
+Console.WriteLine(sonuc);
+sonuc = a==b;
+Console.WriteLine(sonuc);
+sonuc = a!=b;
+Console.WriteLine(sonuc);
+
+Console.WriteLine(" ***** Aritemetik Operatörler ***** ");
+// /, *, +, -,
+
+int sayi1 = 10;
+int sayi2 = 5;
+int sonuc1 = sayi1/sayi2;
+Console.WriteLine(sonuc1);
+sonuc1 = sayi1*sayi2;
+Console.WriteLine(sonuc1);
+sonuc1 = sayi1+sayi2;
+Console.WriteLine(sonuc1);
+sonuc1 = sayi1++;
+Console.WriteLine(sayi1);
+
+// % mod alır
+int sonuc2 = 20%3;
+Console.WriteLine(sonuc2);
